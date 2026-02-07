0416b331176d47feb488243bcd50f1471dddf20c
Juan Mari <juanmarigonzalez2015@gmail.com>
2026-02-07 14:34:09 +0100

    Use Yahoo beacuase is free and concat

--- src/etl/ext.py
+++ src/etl/ext.py
@@ -1,15 +1,45 @@
 #!/usr/bin/env python3
-from alpha_vantage.timeseries import TimeSeries
+import yfinance as yf
 import pandas as pd
+from pathlib import Path
 
-# API Key
-API_KEY = "J0ZKQ51NLRLRAIYC"
+# Carpeta donde guardar los CSV
+output_folder = Path("/mnt/c/Users/juanm/etl/Pipeline-ETL-ML/src/data/entry")
 
-# Inicializa la API
-ts = TimeSeries(key=API_KEY, output_format='pandas')
+# Lista de activos
+stocks = ["AAPL", "TSLA", "MSFT"]
+forex = ["EURUSD=X", "GBPUSD=X", "JPY=X"]
+crypto = ["BTC-USD", "ETH-USD"]
 
-# Obtén datos diarios de una acción (ejemplo: Tesla TSLA)
-data, meta_data = ts.get_daily(symbol='TSLA', outputsize='compact')
+def fetch_and_save(symbols, interval="1d", asset_type="stocks"):
+    filename = output_folder / f"{asset_type}.csv"
+    for symbol in symbols:
+        ticker = yf.Ticker(symbol)
+        data = ticker.history(period="60d", interval=interval)
+        data.index = data.index.date  # índice solo fecha
+        data["Symbol"] = symbol
 
-# Muestra las primeras filas
-print(data.head())
+        if filename.exists():
+            existing_data = pd.read_csv(filename)
+            combined = pd.concat([existing_data, data.reset_index()])
+            # Eliminar duplicados basados en 'index' y 'Symbol', manteniendo el último registro
+            combined = combined.drop_duplicates(subset=["index", "Symbol"], keep="last")
+            combined.to_csv(filename, index=False)
+        else:
+            data.reset_index().to_csv(filename, index=False)
+
+def __main__():
+    
+    # Carpeta donde guardar los CSV
+    output_folder = Path("/mnt/c/Users/juanm/etl/Pipeline-ETL-ML/src/data/entry")
+
+    # Lista de activos
+    stocks = ["AAPL", "TSLA", "MSFT"]
+    forex = ["EURUSD=X", "GBPUSD=X", "JPY=X"]
+    crypto = ["BTC-USD", "ETH-USD"]
+    
+    
+    # Ejecutar ETL
+    fetch_and_save(stocks, interval="1d", asset_type="stocks")
+    fetch_and_save(forex, interval="1d", asset_type="forex")
+    fetch_and_save(crypto, interval="1d", asset_type="crypto")
