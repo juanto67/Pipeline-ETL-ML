b047ec355c8d93b5a59ad08e61943dffb0cabed2
JuantoL <juanmarigonzalez2015@gmail.com>
2026-02-12 15:40:56 +0100

    Change stats_team needs drop columns for the match

--- src/etl/tranform.py
+++ src/etl/tranform.py
@@ -2,7 +2,7 @@
 import logging
 import pandas as pd
 from pathlib import Path
-
+import os
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(name)s: %(message)s",
@@ -11,43 +11,25 @@ logging.basicConfig(
 logger = logging.getLogger("transform")
      
 def stats_team(df):
-    col_home = ["home_score", "home_score_ht", "home_shots_on_target", "home_shots", "home_shots_ht", "home_corners", "home_fouls", "home_yellow", "home_red"]
-    df = df.sort_values(["Home_team", "match_date"])
+    col_home = ["home_score", "home_score_ht", "home_shots_on_target", "home_shots", "home_corners", "home_fouls", "home_yellow", "home_red"]
+    df = df.sort_values(["HomeTeam", "Date"])
     for c in col_home:
-        df["avg_"+c+"_5"] = df.groupby("Home_team")[c].shift(1).rolling(window=5, min_periods=1).mean()
+        df["avg_"+c+"_5"] = df.groupby("HomeTeam")[c].shift(1).rolling(window=5, min_periods=1).mean()
     
-    col_away = ["away_score", "away_score_ht", "away_shots_on_target", "away_shots", "away_shots_ht", "away_corners", "away_fouls", "away_yellow", "away_red"]    
-    df = df.sort_values(["Away_team", "match_date"])
+    col_away = ["away_score", "away_score_ht", "away_shots_on_target", "away_shots", "away_corners", "away_fouls", "away_yellow", "away_red"]    
+    df = df.sort_values(["AwayTeam", "Date"])
     for c in col_away: 
-        df["avg_"+c+"_5"] = df.groupby("Away_team")[c].shift(1).rolling(window=5, min_periods=1).mean()
+        df["avg_"+c+"_5"] = df.groupby("AwayTeam")[c].shift(1).rolling(window=5, min_periods=1).mean()
     
-    home_stats = df.groupby(["Home_team","season_code","league_name"], as_index=False).agg(
-            mean_home_score=("home_score", "mean"),
-            mean_home_score_ht=("home_score_ht", "mean"),
-            mean_home_shots=("home_shots", "mean"),
-            mean_home_shots_on_target=("home_shots_on_target", "mean"),
-            mean_home_corners=("home_corners", "mean"),
-            mean_home_fouls=("home_fouls", "mean"),
-            mean_home_yellow=("home_yellow", "mean"),
-            mean_home_red=("home_red", "mean")
-    )
-    away_stats = df.groupby(["Away_team","season_code","league_name"], as_index=False).agg(
-            mean_away_score=("away_score", "mean"),
-            mean_away_score_ht=("away_score_ht", "mean"),
-            mean_away_shots=("away_shots", "mean"),
-            mean_away_shots_on_target=("away_shots_on_target", "mean"),
-            mean_away_corners=("away_corners", "mean"),
-            mean_away_fouls=("away_fouls", "mean"),
-            mean_away_yellow=("away_yellow", "mean"),
-            mean_away_red=("away_red", "mean")
-    )
-    home_stats = home_stats.rename(columns={"Home_team": "team"})
-    away_stats = away_stats.rename(columns={"Away_team": "team"})
-    team_stats = pd.merge(home_stats, away_stats, on=["team", "season_code", "league_name"], how="outer").fillna(0)
-    return team_stats
+    
+   # home_stats = home_stats.rename(columns={"AwayTeam": "team"})
+   # away_stats = away_stats.rename(columns={"AwayTeam": "team"})
+    #team_stats = pd.merge(home_stats, away_stats, on=["team", "season_code", "league_name"], how="outer").fillna(0)
+    return df
 def __main__():
     entry_folder = Path(__file__).resolve().parents[1] / "data" / "entry"
     output_folder = Path(__file__).resolve().parents[1] / "data" / "proc"
+    os.makedirs(output_folder, exist_ok=True)
     #Read matches data from CSV
     df_liga = pd.read_csv(entry_folder / "matches_liga.csv")
     df_premier = pd.read_csv(entry_folder / "matches_premier.csv")
@@ -62,13 +44,24 @@ def __main__():
 
     for col in colums:
         if col in df_premier.columns:
-            df_premier[col] = df_premier[col].replace(
-                {"H": 0, "D": 1, "A": 2})
+            df_premier[col] = (
+                df_premier[col]
+                .replace({"H": 0, "D": 1, "A": 2})
+                .infer_objects(copy=False)
+            )
         if col in df_liga.columns:
-            df_liga[col] = df_liga[col].replace(
-                {"H": 0, "D": 1, "A": 2})
+            df_liga[col] = (
+                df_liga[col]
+                .replace({"H": 0, "D": 1, "A": 2})
+                .infer_objects(copy=False)
+            )
         if col in df_france.columns:
-            df_france[col] = df_france[col].replace(
-                {"H": 0, "D": 1, "A": 2})       
+            df_france[col] = (
+                df_france[col]
+                .replace({"H": 0, "D": 1, "A": 2})
+                .infer_objects(copy=False)
+            )
+    df_liga = stats_team(df_liga)
+    df_liga.to_csv(output_folder / "matches_liga.csv", index=False)      
 if __name__ == "__main__":
     __main__()
