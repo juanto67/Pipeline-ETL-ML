9340bb8813277e712f09e394ceb6fc693aa357a2
Juan Mari <juanmarigonzalez2015@gmail.com>
2026-02-13 18:32:17 +0100

    Drop duplicate and elo rating and diff

--- src/etl/tranform.py
+++ src/etl/tranform.py
@@ -11,12 +11,21 @@ logging.basicConfig(
 logger = logging.getLogger("transform")
 
 def add_elo(df, k=10, base_elo=100):
+    df = df.sort_values(["season_code", "division", "Date", "HomeTeam", "AwayTeam"])
     df = df.copy()
-    df["elo"] = base_elo
-    df["elo_away"] = base_elo
+    df["elo"] = float(base_elo)
+    df["elo_away"] = float(base_elo)
     ratings = {}
-
+    
+    season = None
+    division = None
     for i, row in df.iterrows():
+        if (
+            season is not None
+            and division is not None
+            and (season != row["season_code"] or division != row["division"])
+        ):
+            ratings = {}
         home_team = row["HomeTeam"]
         away_team = row["AwayTeam"]
 
@@ -37,10 +46,11 @@ def add_elo(df, k=10, base_elo=100):
             home_score, away_score = 0.5, 0.5
         else:
             home_score, away_score = 0.0, 1.0
-
-        ratings[home_team] = home_elo + k * (home_score - expected_home)
-        ratings[away_team] = away_elo + k * (away_score - expected_away)
-
+        division = row["division"]
+        season = row["season_code"]
+        ratings[home_team] = round(home_elo + k * (home_score - expected_home), 3)
+        ratings[away_team] = round(away_elo + k * (away_score - expected_away), 3)
+    df["elo_diff"] = (df["elo"] - df["elo_away"]).round(3)
     return df
 def merge_and_save(df, filename, dedupe_keys, sort_keys):
     if filename.exists():
@@ -68,6 +78,7 @@ def stats_team(df):
         )
         
     col_away = ["away_score", "away_score_ht", "away_shots_on_target", "away_shots", "away_corners", "away_fouls", "away_yellow", "away_red"]    
+    df = df.sort_values(["season_code","Date","AwayTeam"])
     for c in col_away: 
         df["avg_"+c+"_5"] = (
             df.groupby("AwayTeam")[c]
@@ -106,14 +117,21 @@ def __main__():
         if col in df_france.columns:
             df_france[col] = df_france[col].map({"H": 0, "D": 1, "A": 2})
             pd.to_numeric(df_france[col], errors="coerce").astype("Int64")
+
+    dedupe_keys = ["season_code", "division", "Date", "HomeTeam", "AwayTeam"]
+    df_liga = df_liga.drop_duplicates(subset=dedupe_keys, keep="last").reset_index(drop=True)
+    df_premier = df_premier.drop_duplicates(subset=dedupe_keys, keep="last").reset_index(drop=True)
+    df_france = df_france.drop_duplicates(subset=dedupe_keys, keep="last").reset_index(drop=True)
             
     df_liga = stats_team(df_liga)
     df_premier = stats_team(df_premier)
     df_france = stats_team(df_france)
-    
-    merge_and_save(df_liga, output_folder / "matches_liga.csv", dedupe_keys=["season_code", "division", "Date", "HomeTeam", "AwayTeam"], sort_keys=["season_code", "division", "Date", "HomeTeam", "AwayTeam"])
-    merge_and_save(df_premier, output_folder / "matches_premier.csv", dedupe_keys=["season_code", "division", "Date", "HomeTeam", "AwayTeam"], sort_keys=["season_code", "division", "Date", "HomeTeam", "AwayTeam"])
-    merge_and_save(df_france, output_folder / "matches_france.csv", dedupe_keys=["season_code", "division", "Date", "HomeTeam", "AwayTeam"], sort_keys=["season_code", "division", "Date", "HomeTeam", "AwayTeam"])
+    df_liga = add_elo(df_liga)
+    df_premier = add_elo(df_premier)
+    df_france = add_elo(df_france)
+    merge_and_save(df_liga, output_folder / "matches_liga.csv", dedupe_keys=dedupe_keys, sort_keys=["season_code", "division", "Date", "HomeTeam", "AwayTeam"])
+    merge_and_save(df_premier, output_folder / "matches_premier.csv", dedupe_keys=dedupe_keys, sort_keys=["season_code", "division", "Date", "HomeTeam", "AwayTeam"])
+    merge_and_save(df_france, output_folder / "matches_france.csv", dedupe_keys=dedupe_keys, sort_keys=["season_code", "division", "Date", "HomeTeam", "AwayTeam"])
     
     
     
